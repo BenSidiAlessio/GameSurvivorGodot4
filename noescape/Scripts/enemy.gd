3b2f90bb5f434bf4a9b52fc3cf9be1d0dd2aac34
BenSidiAlessio <bensidialessio@gmail.com>
2024-11-11 10:53:07 +0100

    added YellowTRex as enemy

--- noescape/Scripts/enemy.gd
+++ noescape/Scripts/enemy.gd
@@ -1,10 +1,16 @@
-extends Node2D
+extends Node
 
 @export var enemy : Enemy 
+@export var enemies = {}
 
-
-func spawn_mob(level):
-	var mob = enemy.scene.instantiate()
+func spawn_mob(level : int):
+	var mob
+	
+	if level >= 3:
+		mob = enemies["green_slime"].scene.instantiate()
+	else:	
+		mob = enemies["yellow_trex"].scene.instantiate()
+		
 	%SpawnerPosition.progress_ratio = randf()
 	mob.global_position = %SpawnerPosition.global_position
 	add_child(mob)
